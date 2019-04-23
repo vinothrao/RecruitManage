66ba9622ba5061afafb63342871c9250c5303f9f
sysadmin <sysadmin@windows>
2019-04-23 18:40:02 +0000

    nginx proxy with multi container

--- RecruitManageAPI/Startup.cs
+++ RecruitManageAPI/Startup.cs
@@ -25,7 +25,15 @@ namespace RecruitManageAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
+            {
+                builder.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowAnyHeader();
+            }));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+           
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,8 +48,9 @@ namespace RecruitManageAPI
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+            app.UseCors("MyPolicy");
             app.UseHttpsRedirection();
+            
             app.UseMvc();
         }
     }
